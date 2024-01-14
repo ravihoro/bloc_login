995225068a403fe7a8d07ac93c4db05ceb10414b
ravihoro <ravijohnhoro5@gmail.com>
2024-01-15 00:02:22 +0530

    Added sign up test

--- test/features/authentication/data/authentication_data_source_impl_test.dart
+++ test/features/authentication/data/authentication_data_source_impl_test.dart
@@ -14,7 +14,9 @@ void main() {
 
   var dataSource = AuthenticationDataSourceImpl(http);
 
-  var uri = Uri.parse("http://localhost:3000/login");
+  var loginUri = Uri.parse("http://localhost:3000/login");
+
+  var signUpUri = Uri.parse("http://localhost:3000/signUp");
 
   var userModel = UserModel(
     id: "1",
@@ -23,46 +25,105 @@ void main() {
     password: "123456",
   );
 
-  var body =
-      jsonEncode({"email": userModel.email, "password": userModel.password});
+  var loginBody = jsonEncode({
+    "email": userModel.email,
+    "password": userModel.password,
+  });
+
+  var signUpBody = jsonEncode({
+    "email": userModel.email,
+    "password": userModel.password,
+    "name": "ravi"
+  });
 
   var successResponse = Response(jsonEncode(userModel.toJson()), 200);
 
+  var successSignUpResponse = Response(jsonEncode(userModel.toJson()), 201);
+
   var failedResponse = Response("Error", 400);
 
-  test(
-    'should be a successful login',
-    () async {
-      when(() => http.post(
-            uri,
-            body: body,
-          )).thenAnswer(
-        (_) async => successResponse,
-      );
+  group('should test login', () {
+    test(
+      'should be a successful login',
+      () async {
+        when(() => http.post(
+              loginUri,
+              body: loginBody,
+            )).thenAnswer(
+          (_) async => successResponse,
+        );
 
-      var response = await dataSource.login(
-        email: userModel.email,
-        password: userModel.password,
-      );
-
-      expect(response, userModel);
-    },
-  );
-
-  test(
-    'should be a login exception',
-    () async {
-      when(() => http.post(uri, body: body))
-          .thenAnswer((_) async => failedResponse);
-
-      expect(
-        () async => await dataSource.login(
+        var response = await dataSource.login(
           email: userModel.email,
           password: userModel.password,
-        ),
-        throwsA(
-          isA<LoginException>(),
-        ),
+        );
+
+        verify(() => http.post(loginUri, body: loginBody)).called(1);
+
+        expect(response, userModel);
+      },
+    );
+
+    test(
+      'should be a login exception',
+      () async {
+        when(() => http.post(loginUri, body: loginBody))
+            .thenAnswer((_) async => failedResponse);
+
+        expect(
+          dataSource.login(
+            email: userModel.email,
+            password: userModel.password,
+          ),
+          throwsA(
+            isA<LoginException>(),
+          ),
+        );
+      },
+    );
+  });
+
+  group(
+    'should test sign up',
+    () {
+      test(
+        'should be a successful sign up',
+        () async {
+          when(() => http.post(
+                signUpUri,
+                body: signUpBody,
+              )).thenAnswer(
+            (_) async => successSignUpResponse,
+          );
+
+          var response = await dataSource.signUp(
+            email: userModel.email,
+            password: userModel.password,
+            name: userModel.name,
+          );
+
+          verify(() => http.post(signUpUri, body: signUpBody)).called(1);
+
+          expect(response, userModel);
+        },
+      );
+
+      test(
+        'should be a sign up exception',
+        () async {
+          when(() => http.post(signUpUri, body: signUpBody))
+              .thenAnswer((_) => throw SignUpException());
+
+          expect(
+            dataSource.signUp(
+                email: userModel.email,
+                password: userModel.password,
+                name: userModel.name),
+            throwsA(
+              isA<SignUpException>(),
+            ),
+          );
+        },
       );
     },
   );
