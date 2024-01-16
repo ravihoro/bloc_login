a2878d20f7f851b3245ef51f8929205202573afa
ravihoro <ravijohnhoro5@gmail.com>
2024-01-16 22:58:15 +0530

    Added isLoading variable

--- lib/features/authentication/presentation/cubit/authentication_state.dart
+++ lib/features/authentication/presentation/cubit/authentication_state.dart
@@ -2,19 +2,30 @@ part of 'authentication_cubit.dart';
 
 class AuthenticationState extends Equatable {
   final User user;
+  final bool isLoading;
   final String error;
 
-  const AuthenticationState({this.user = User.empty, this.error = ""});
+  const AuthenticationState({
+    this.user = User.empty,
+    this.error = "",
+    this.isLoading = false,
+  });
 
-  AuthenticationState copyWith({User user = User.empty, String error = ""}) =>
+  AuthenticationState copyWith({
+    User? user,
+    String error = "",
+    bool isLoading = false,
+  }) =>
       AuthenticationState(
-        user: user,
+        user: user ?? this.user,
         error: error,
+        isLoading: isLoading,
       );
 
   @override
   List<Object?> get props => [
         user,
         error,
+        isLoading,
       ];
 }
