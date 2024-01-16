df12c45e838e74cfb8d7d2fce7d8896e9a7c4d37
ravihoro <ravijohnhoro5@gmail.com>
2024-01-16 22:59:37 +0530

    Commented code

--- test/features/login/presentation/login_cubit_test.dart
+++ test/features/login/presentation/login_cubit_test.dart
@@ -1,107 +1,107 @@
-import 'package:bloc_login/core/error/failure.dart';
-import 'package:bloc_login/core/model/user_model.dart';
-import 'package:bloc_login/features/authentication/domain/entity/user.dart';
-import 'package:bloc_login/features/login/domain/usecase/login_usecase.dart';
-import 'package:bloc_login/features/login/presentation/bloc/login_cubit.dart';
-import 'package:bloc_login/features/login/presentation/bloc/login_state.dart';
-import 'package:bloc_test/bloc_test.dart';
-import 'package:dartz/dartz.dart';
-import 'package:mocktail/mocktail.dart';
+// import 'package:bloc_login/core/error/failure.dart';
+// import 'package:bloc_login/core/model/user_model.dart';
+// import 'package:bloc_login/features/authentication/domain/entity/user.dart';
+// import 'package:bloc_login/features/login/domain/usecase/login_usecase.dart';
+// import 'package:bloc_login/features/login/presentation/bloc/login_cubit.dart';
+// import 'package:bloc_login/features/login/presentation/bloc/login_state.dart';
+// import 'package:bloc_test/bloc_test.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:mocktail/mocktail.dart';
 
-class MockLoginUseCase extends Mock implements LoginUseCase {}
+// class MockLoginUseCase extends Mock implements LoginUseCase {}
 
-void main() {
-  MockLoginUseCase usecase = MockLoginUseCase();
-  LoginCubit loginCubit = LoginCubit(useCase: usecase);
+// void main() {
+//   MockLoginUseCase usecase = MockLoginUseCase();
+//   LoginCubit loginCubit = LoginCubit(useCase: usecase);
 
-  String username = "ravijohn";
-  String email = "ravi.horo@gmail.com";
-  String password = "Ravi@1234";
+//   String username = "ravijohn";
+//   String email = "ravi.horo@gmail.com";
+//   String password = "Ravi@1234";
 
-  User user = User(
-    email: email,
-    name: username,
-    id: "",
-    password: "",
-  );
-  var loginFailure = const LoginFailure();
-  var serverFailure = const ServerFailure();
-  var failure = const GenericFailure();
+//   User user = User(
+//     email: email,
+//     name: username,
+//     id: "",
+//     password: "",
+//   );
+//   var loginFailure = const LoginFailure();
+//   var serverFailure = const ServerFailure();
+//   var failure = const GenericFailure();
 
-  blocTest<LoginCubit, LoginState>(
-    "should be a successful login",
-    build: () => loginCubit,
-    act: (loginCubit) {
-      when(() => usecase.login(email: email, password: password))
-          .thenAnswer((_) async => Right(user));
+//   blocTest<LoginCubit, LoginState>(
+//     "should be a successful login",
+//     build: () => loginCubit,
+//     act: (loginCubit) {
+//       when(() => usecase.login(email: email, password: password))
+//           .thenAnswer((_) async => Right(user));
 
-      loginCubit.login(email: email, password: password);
+//       loginCubit.login(email: email, password: password);
 
-      verify(() => usecase.login(email: email, password: password));
-    },
-    expect: (() => [
-          const LoginState(isLoading: true),
-          LoginState(user: user, isLoading: false),
-        ]),
-  );
+//       verify(() => usecase.login(email: email, password: password));
+//     },
+//     expect: (() => [
+//           const LoginState(isLoading: true),
+//           LoginState(user: user, isLoading: false),
+//         ]),
+//   );
 
-  blocTest<LoginCubit, LoginState>(
-    "should be a failed login",
-    build: () => loginCubit,
-    act: (loginCubit) {
-      when(() => usecase.login(email: email, password: password))
-          .thenAnswer((_) async => Left(loginFailure));
+//   blocTest<LoginCubit, LoginState>(
+//     "should be a failed login",
+//     build: () => loginCubit,
+//     act: (loginCubit) {
+//       when(() => usecase.login(email: email, password: password))
+//           .thenAnswer((_) async => Left(loginFailure));
 
-      loginCubit.login(email: email, password: password);
+//       loginCubit.login(email: email, password: password);
 
-      verify(() => usecase.login(email: email, password: password));
-    },
-    expect: (() => [
-          const LoginState(isLoading: true),
-          LoginState(
-            isLoading: false,
-            errorString: loginFailure.error,
-          )
-        ]),
-  );
+//       verify(() => usecase.login(email: email, password: password));
+//     },
+//     expect: (() => [
+//           const LoginState(isLoading: true),
+//           LoginState(
+//             isLoading: false,
+//             errorString: loginFailure.error,
+//           )
+//         ]),
+//   );
 
-  blocTest<LoginCubit, LoginState>(
-    "should be a server exception",
-    build: () => loginCubit,
-    act: (loginCubit) {
-      when(() => usecase.login(email: email, password: password))
-          .thenAnswer((_) async => Left(serverFailure));
+//   blocTest<LoginCubit, LoginState>(
+//     "should be a server exception",
+//     build: () => loginCubit,
+//     act: (loginCubit) {
+//       when(() => usecase.login(email: email, password: password))
+//           .thenAnswer((_) async => Left(serverFailure));
 
-      loginCubit.login(email: email, password: password);
+//       loginCubit.login(email: email, password: password);
 
-      verify(() => usecase.login(email: email, password: password));
-    },
-    expect: (() => [
-          const LoginState(isLoading: true),
-          LoginState(
-            isLoading: false,
-            errorString: serverFailure.error,
-          )
-        ]),
-  );
+//       verify(() => usecase.login(email: email, password: password));
+//     },
+//     expect: (() => [
+//           const LoginState(isLoading: true),
+//           LoginState(
+//             isLoading: false,
+//             errorString: serverFailure.error,
+//           )
+//         ]),
+//   );
 
-  blocTest<LoginCubit, LoginState>(
-    "should be an exception",
-    build: () => loginCubit,
-    act: (loginCubit) {
-      when(() => usecase.login(email: email, password: password))
-          .thenAnswer((_) async => Left(failure));
+//   blocTest<LoginCubit, LoginState>(
+//     "should be an exception",
+//     build: () => loginCubit,
+//     act: (loginCubit) {
+//       when(() => usecase.login(email: email, password: password))
+//           .thenAnswer((_) async => Left(failure));
 
-      loginCubit.login(email: email, password: password);
+//       loginCubit.login(email: email, password: password);
 
-      verify(() => usecase.login(email: email, password: password));
-    },
-    expect: (() => [
-          const LoginState(isLoading: true),
-          LoginState(
-            isLoading: false,
-            errorString: failure.error,
-          )
-        ]),
-  );
-}
+//       verify(() => usecase.login(email: email, password: password));
+//     },
+//     expect: (() => [
+//           const LoginState(isLoading: true),
+//           LoginState(
+//             isLoading: false,
+//             errorString: failure.error,
+//           )
+//         ]),
+//   );
+// }
--- test/features/sign_up/presentation/bloc/sign_up_cubit_test.dart
+++ test/features/sign_up/presentation/bloc/sign_up_cubit_test.dart
@@ -1,114 +1,114 @@
-import 'package:bloc_login/core/error/failure.dart';
-import 'package:bloc_login/core/model/user_model.dart';
-import 'package:bloc_login/features/authentication/domain/entity/user.dart';
-import 'package:bloc_login/features/sign_up/domain/usecase/sign_up_usecase.dart';
-import 'package:bloc_login/features/sign_up/presentation/bloc/sign_up_cubit.dart';
-import 'package:bloc_login/features/sign_up/presentation/bloc/sign_up_state.dart';
+// import 'package:bloc_login/core/error/failure.dart';
+// import 'package:bloc_login/core/model/user_model.dart';
+// import 'package:bloc_login/features/authentication/domain/entity/user.dart';
+// import 'package:bloc_login/features/sign_up/domain/usecase/sign_up_usecase.dart';
+// import 'package:bloc_login/features/sign_up/presentation/bloc/sign_up_cubit.dart';
+// import 'package:bloc_login/features/sign_up/presentation/bloc/sign_up_state.dart';
 
-import 'package:bloc_test/bloc_test.dart';
-import 'package:dartz/dartz.dart';
-import 'package:mocktail/mocktail.dart';
+// import 'package:bloc_test/bloc_test.dart';
+// import 'package:dartz/dartz.dart';
+// import 'package:mocktail/mocktail.dart';
 
-class MockSignUpUsecase extends Mock implements SignUpUsecase {}
+// class MockSignUpUsecase extends Mock implements SignUpUsecase {}
 
-void main() {
-  MockSignUpUsecase usecase = MockSignUpUsecase();
+// void main() {
+//   MockSignUpUsecase usecase = MockSignUpUsecase();
 
-  String username = "ravijohn";
-  String email = "ravi.horo@gmail.com";
-  String password = "Ravi@1234";
+//   String username = "ravijohn";
+//   String email = "ravi.horo@gmail.com";
+//   String password = "Ravi@1234";
 
-  User user = User(
-    id: "",
-    password: "",
-    name: username,
-    email: email,
-  );
+//   User user = User(
+//     id: "",
+//     password: "",
+//     name: username,
+//     email: email,
+//   );
 
-  Either<Failure, User> successfulSignUp = Right(
-    user,
-  );
+//   Either<Failure, User> successfulSignUp = Right(
+//     user,
+//   );
 
-  var signUpFailure = const SignUpFailure();
-  var serverFailure = const ServerFailure();
-  var failure = const GenericFailure();
+//   var signUpFailure = const SignUpFailure();
+//   var serverFailure = const ServerFailure();
+//   var failure = const GenericFailure();
 
-  blocTest<SignUpCubit, SignUpState>(
-    'Should be a successful login',
-    build: () => SignUpCubit(usecase: usecase),
-    act: (signUpCubit) {
-      when(() => usecase.signUp(
-              username: username, email: email, password: password))
-          .thenAnswer((realInvocation) async => successfulSignUp);
+//   blocTest<SignUpCubit, SignUpState>(
+//     'Should be a successful login',
+//     build: () => SignUpCubit(usecase: usecase),
+//     act: (signUpCubit) {
+//       when(() => usecase.signUp(
+//               username: username, email: email, password: password))
+//           .thenAnswer((realInvocation) async => successfulSignUp);
 
-      signUpCubit.signUp(username: username, email: email, password: password);
+//       signUpCubit.signUp(username: username, email: email, password: password);
 
-      verify(() =>
-          usecase.signUp(username: username, email: email, password: password));
-    },
-    expect: () => [
-      const SignUpState(isLoading: true),
-      SignUpState(
-        isLoading: false,
-        user: user,
-      ),
-    ],
-  );
+//       verify(() =>
+//           usecase.signUp(username: username, email: email, password: password));
+//     },
+//     expect: () => [
+//       const SignUpState(isLoading: true),
+//       SignUpState(
+//         isLoading: false,
+//         user: user,
+//       ),
+//     ],
+//   );
 
-  blocTest<SignUpCubit, SignUpState>(
-    'Should be a failed login',
-    build: () => SignUpCubit(usecase: usecase),
-    act: (signUpCubit) {
-      when(() => usecase.signUp(
-              username: username, email: email, password: password))
-          .thenAnswer((realInvocation) async => Left(signUpFailure));
+//   blocTest<SignUpCubit, SignUpState>(
+//     'Should be a failed login',
+//     build: () => SignUpCubit(usecase: usecase),
+//     act: (signUpCubit) {
+//       when(() => usecase.signUp(
+//               username: username, email: email, password: password))
+//           .thenAnswer((realInvocation) async => Left(signUpFailure));
 
-      signUpCubit.signUp(username: username, email: email, password: password);
+//       signUpCubit.signUp(username: username, email: email, password: password);
 
-      verify(() =>
-          usecase.signUp(username: username, email: email, password: password));
-    },
-    expect: () => [
-      const SignUpState(isLoading: true),
-      SignUpState(isLoading: false, errorString: signUpFailure.error),
-    ],
-  );
+//       verify(() =>
+//           usecase.signUp(username: username, email: email, password: password));
+//     },
+//     expect: () => [
+//       const SignUpState(isLoading: true),
+//       SignUpState(isLoading: false, errorString: signUpFailure.error),
+//     ],
+//   );
 
-  blocTest<SignUpCubit, SignUpState>(
-    'Should be a server exception',
-    build: () => SignUpCubit(usecase: usecase),
-    act: (signUpCubit) {
-      when(() => usecase.signUp(
-              username: username, email: email, password: password))
-          .thenAnswer((realInvocation) async => Left(serverFailure));
+//   blocTest<SignUpCubit, SignUpState>(
+//     'Should be a server exception',
+//     build: () => SignUpCubit(usecase: usecase),
+//     act: (signUpCubit) {
+//       when(() => usecase.signUp(
+//               username: username, email: email, password: password))
+//           .thenAnswer((realInvocation) async => Left(serverFailure));
 
-      signUpCubit.signUp(username: username, email: email, password: password);
+//       signUpCubit.signUp(username: username, email: email, password: password);
 
-      verify(() =>
-          usecase.signUp(username: username, email: email, password: password));
-    },
-    expect: () => [
-      const SignUpState(isLoading: true),
-      SignUpState(isLoading: false, errorString: serverFailure.error),
-    ],
-  );
+//       verify(() =>
+//           usecase.signUp(username: username, email: email, password: password));
+//     },
+//     expect: () => [
+//       const SignUpState(isLoading: true),
+//       SignUpState(isLoading: false, errorString: serverFailure.error),
+//     ],
+//   );
 
-  blocTest<SignUpCubit, SignUpState>(
-    'Should be an exception',
-    build: () => SignUpCubit(usecase: usecase),
-    act: (signUpCubit) {
-      when(() => usecase.signUp(
-              username: username, email: email, password: password))
-          .thenAnswer((realInvocation) async => Left(failure));
+//   blocTest<SignUpCubit, SignUpState>(
+//     'Should be an exception',
+//     build: () => SignUpCubit(usecase: usecase),
+//     act: (signUpCubit) {
+//       when(() => usecase.signUp(
+//               username: username, email: email, password: password))
+//           .thenAnswer((realInvocation) async => Left(failure));
 
-      signUpCubit.signUp(username: username, email: email, password: password);
+//       signUpCubit.signUp(username: username, email: email, password: password);
 
-      verify(() =>
-          usecase.signUp(username: username, email: email, password: password));
-    },
-    expect: () => [
-      const SignUpState(isLoading: true),
-      SignUpState(isLoading: false, errorString: failure.error),
-    ],
-  );
-}
+//       verify(() =>
+//           usecase.signUp(username: username, email: email, password: password));
+//     },
+//     expect: () => [
+//       const SignUpState(isLoading: true),
+//       SignUpState(isLoading: false, errorString: failure.error),
+//     ],
+//   );
+// }
