f72f04fcee508b1393bf93e3ce05f46bf7b5eb05
ravihoro <ravijohnhoro5@gmail.com>
2024-03-09 19:19:55 +0530

    text field changes

---
+++ lib/core/widgets/custom_text_form_field.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  final String labelText;
+  final String fieldKey;
+  final Function(String?)? onChanged;
+  final String? Function(String?)? validator;
+  final bool obscureText;
+
+  const CustomTextFormField({
+    required this.labelText,
+    required this.fieldKey,
+    this.onChanged,
+    this.validator,
+    this.obscureText = false,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      key: Key(fieldKey),
+      onChanged: onChanged,
+      validator: validator,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+        labelText: labelText,
+        //errorText: isValidEmail(state.email),
+      ),
+    );
+  }
+}
