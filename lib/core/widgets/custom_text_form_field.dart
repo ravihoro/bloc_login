e1cc2f002fbabd2586c0d63b330ff552b9dcca39
ravihoro <ravijohnhoro5@gmail.com>
2024-03-09 22:11:25 +0530

    Added key field

--- lib/core/widgets/custom_text_form_field.dart
+++ lib/core/widgets/custom_text_form_field.dart
@@ -19,7 +19,7 @@ class CustomTextFormField extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return TextFormField(
-      key: Key(fieldKey),
+      key: ValueKey(fieldKey),
       onChanged: onChanged,
       validator: validator,
       obscureText: obscureText,
