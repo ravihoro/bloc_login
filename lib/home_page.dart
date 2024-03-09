3b99444afa095a15da76366535b7416f67a356ca
ravihoro <ravijohnhoro5@gmail.com>
2024-03-09 19:20:35 +0530

    user name

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,13 +12,23 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: TextButton(
-          child: Text('Logout'),
-          onPressed: () async {
-            SharedPreferences prefs = await SharedPreferences.getInstance();
-            prefs.setString('user', '');
-            context.read<AuthenticationCubit>().logout();
-          },
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocBuilder<AuthenticationCubit, AuthenticationState>(
+              builder: (context, state) {
+                return Text(state.user.name);
+              },
+            ),
+            TextButton(
+              child: Text('Logout'),
+              onPressed: () async {
+                SharedPreferences prefs = await SharedPreferences.getInstance();
+                prefs.setString('user', '');
+                context.read<AuthenticationCubit>().logout();
+              },
+            ),
+          ],
         ),
       ),
     );
