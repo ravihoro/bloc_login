a00a527904129ee28a06746819abeb588e3c48ac
ravihoro <ravijohnhoro5@gmail.com>
2024-03-09 22:11:01 +0530

    Changes for mock

--- lib/core/utils/injection_container.dart
+++ lib/core/utils/injection_container.dart
@@ -1,6 +1,5 @@
 import 'package:bloc_login/features/authentication/data/data_source/authentication_data_source.dart';
 import 'package:bloc_login/features/authentication/data/repository/authentication_repository_impl.dart';
-import 'package:bloc_login/features/authentication/domain/repository/authentication_repository.dart';
 import 'package:bloc_login/features/authentication/domain/usecase/login.dart';
 import 'package:bloc_login/features/authentication/domain/usecase/sign_up.dart';
 import 'package:bloc_login/features/authentication/presentation/cubit/authentication_cubit.dart';
@@ -11,17 +10,24 @@ import 'package:http/http.dart' as http;
 
 final sl = GetIt.instance;
 
-Future<void> init() async {
-  sl.registerFactory(() => SignUpCubit());
-  sl.registerFactory(() => LoginCubit());
-  sl.registerFactory(
-      () => AuthenticationCubit(login: sl<Login>(), signUp: sl<SignUp>()));
+Future<void> init({
+  bool isMock = false,
+}) async {
+  if (isMock) {
+    sl.registerFactory(() => SignUpCubit());
+    sl.registerFactory(() => LoginCubit());
+  } else {
+    sl.registerFactory(() => SignUpCubit());
+    sl.registerFactory(() => LoginCubit());
+    sl.registerFactory(
+        () => AuthenticationCubit(login: sl<Login>(), signUp: sl<SignUp>()));
 
-  sl.registerLazySingleton(() => Login(sl<AuthenticationRepositoryImpl>()));
-  sl.registerLazySingleton(() => SignUp(sl<AuthenticationRepositoryImpl>()));
-  sl.registerLazySingleton(
-      () => AuthenticationRepositoryImpl(sl<AuthenticationDataSourceImpl>()));
-  sl.registerLazySingleton(
-      () => AuthenticationDataSourceImpl(sl<http.Client>()));
-  sl.registerLazySingleton(() => http.Client());
+    sl.registerLazySingleton(() => Login(sl<AuthenticationRepositoryImpl>()));
+    sl.registerLazySingleton(() => SignUp(sl<AuthenticationRepositoryImpl>()));
+    sl.registerLazySingleton(
+        () => AuthenticationRepositoryImpl(sl<AuthenticationDataSourceImpl>()));
+    sl.registerLazySingleton(
+        () => AuthenticationDataSourceImpl(sl<http.Client>()));
+    sl.registerLazySingleton(() => http.Client());
+  }
 }
